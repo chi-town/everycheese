b535edc936c88539d470ca26e88dcfbd81da6315
Matt Simpson <recurse.then.reverse@gmail.com>
2020-08-23 16:10:50 -0500

    added factory and get absolute url test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,14 +1,16 @@
 import pytest 
+from .factories import CheeseFactory
 
 pytestmark = pytest.mark.django_db
 
 from ..models import Cheese
 
 def test__str__():
-    cheese = Cheese.objects.create(
-        name="gross",
-        description="old squishy cheese",
-        firmness=Cheese.Firmness.SOFT,
-    )
-    assert cheese.__str__() == "gross"
-    assert str(cheese) == "gross"
+    cheese = CheeseFactory(name="Stracchino")
+    assert cheese.__str__() == "Stracchino"
+    assert str(cheese) == "Stracchino"
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
