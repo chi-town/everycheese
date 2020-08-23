d0a933dc3951614f42c75a33b03decf71c6dbf22
Matt Simpson <recurse.then.reverse@gmail.com>
2020-08-23 15:29:21 -0500

    require login

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Cheese
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class CheeseListView(ListView):
@@ -9,7 +10,7 @@ class CheeseListView(ListView):
 class CheeseDetailView(DetailView):
     model = Cheese
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
 
     fields = [
