96922d81c293bf31d7e186cd1b56c8d2a7297cfa
Matt Simpson <recurse.then.reverse@gmail.com>
2020-08-23 17:32:03 -0500

    added update screen, template

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Cheese
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -23,3 +23,13 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cheese
+    fields = [
+        'name',
+        'description',
+        'firmness',
+        'country_of_origin'
+    ]
+    action = "Update"
